8df29951a9a2a0607320551072900d890c9c724c
DhulipallaDeepthi <120773894+DhulipallaDeepthi@users.noreply.github.com>
2023-01-07 15:32:09 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,35 @@
 x = input("Type a number: ")
 y = input("Type another number: ")
-
 sum = int(x) + int(y)
-
 print("The sum is: ", sum)
+trigger:
+- main
+
+pool:
+  vmImage: ubuntu-latest
+strategy:
+  matrix:
+    Python27:
+      python.version: '2.7'
+    Python35:
+      python.version: '3.5'
+    Python36:
+      python.version: '3.6'
+    Python37:
+      python.version: '3.7'
+
+steps:
+- task: UsePythonVersion@0
+  inputs:
+    versionSpec: '$(python.version)'
+  displayName: 'Use Python $(python.version)'
+
+- script: |
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+  displayName: 'Install dependencies'
+
+- script: |
+    pip install pytest pytest-azurepipelines
+    pytest
+  displayName: 'pytest'
